2b40f5416ccaead229c7cf8006135f290da516c6
Andreas Beasley <andreas@sapioweb.com>
2016-02-11 01:26:04 -0800

    todo updated, roles created, minor adjustments

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,72 +12,134 @@ use App\Product;
 class AdminController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    * Create a new controller instance.
+    *
+    * @return void
+    */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
     /**
-     * Show the application dashboard.
-     *
-     * @return Response
-     */
+    * Show the application dashboard.
+    *
+    * @return Response
+    */
     public function dashboard()
     {
-        $orders = CrudHelper::index(new \App\Order);
+        $orders = $this->getOrders();
+
+        $products = $this->getProducts();
+
+        $totalRevenue = $this->calculateRevenue($orders);
+
+        $productCount = count($products);
 
         return view('admin.dashboard')->with([
-            'orders' => $orders
+            'orders' => $orders,
+            'productCount' => $productCount,
+            'totalRevenue' => $totalRevenue
         ]);
     }
 
     /**
-     * Show the application orders.
-     *
-     * @return Response
-     */
+    * Show the application orders.
+    *
+    * @return Response
+    */
     public function showOrders()
     {
-        $orders = CrudHelper::index(new \App\Order);
+        $orders = $this->getOrders();
 
         return view('admin.orders.orders')->with([
             'orders' => $orders
         ]);
     }
 
-    /**
-     * Show the application products.
-     *
-     * @return Response
-     */
-    public function showProducts()
+    public function getOrders()
+    {
+        $orders = CrudHelper::index(new \App\Order, ['products']);
+
+        return $orders->orderBy('created_at', 'DESC')->get();
+    }
+
+    public function getProducts()
     {
         $products = CrudHelper::index(new \App\Product);
+
+        return $products->get();
+    }
+
+    public function calculateRevenue($orders)
+    {
+        foreach ($orders as $order) {
+            $amounts[] = $order['amount'];
+        }
+
+        return array_sum($amounts);
+    }
+
+    /**
+    * Show the application products.
+    *
+    * @return Response
+    */
+    public function showProducts()
+    {
+        $products = CrudHelper::index(new \App\Product, ['images'])->get();
+
+        foreach ($products as $productKey => $productValue) {
+            if (! isset($products[$productKey]['images'][0])) {
+                unset($products[$productKey]);
+            }
+        }
 
         return view('admin.products.products')->with([
             'products' => $products
         ]);
     }
 
-    public function editProducts($id)
+    public function createProducts(Request $request)
     {
-        $product = CrudHelper::show(new \App\Product, 'id', $id);
+        $categories =$this->categoriesSelect();
 
-        return view('admin.products.edit')->with([
-            'product' => $product
+        return view('admin.products.create')->with([
+            'categories' => $categories
         ]);
     }
 
-    public function createProducts(Request $request)
+    public function categoriesSelect()
+    {
+        $categories = CrudHelper::index(new \App\Category)->get()->toArray();
+
+        foreach ($categories as $category) {
+            $categoriesSelect[$category['id']] = $category['category'];
+        }
+
+        $categoriesSelect[0] = 'Select Category';
+
+        return $categoriesSelect;
+    }
+
+    public function editProducts($id)
+    {
+        $categories =$this->categoriesSelect();
+
+        $product = CrudHelper::show(new \App\Product, 'id', $id)->first();
+
+        return view('admin.products.edit')->with([
+            'product' => $product,
+            'categories' => $categories
+        ]);
+    }
+
+    public function updateProducts(Request $request, $id)
     {
         dd($request);
     }
 
-    public function updateProducts(Request $request, $id)
+    public function storeProducts(Request $request, $id)
     {
         dd($request);
     }
