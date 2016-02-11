5c7efa4ed66d060c893a222e2bf7b588b76c6d32
Andreas Beasley <andreas@sapioweb.com>
2016-02-11 01:28:11 -0800

    new seeder edits, and migrations

---
+++ database/migrations/2016_02_07_172250_add_is_sub_cat_to_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsSubCatToCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->integer('is_sub_cat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('is_sub_cat');
+        });
+    }
+}
