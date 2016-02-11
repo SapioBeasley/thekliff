4f495c39e135989f58cbdd96e91bca2cca627021
Andreas Beasley <andreas@sapioweb.com>
2016-02-11 01:24:49 -0800

    todo updated, roles created, minor adjustments

--- resources/views/includes/freeShippingBox.blade.php
+++ resources/views/includes/freeShippingBox.blade.php
@@ -1,6 +1,6 @@
 <div class="box red">
 	<div class="simple-box">
-		<a href="index-2.html#" class="underline">
+		<a href="{{url('/shop')}}" class="underline">
 			<span class="small-text">FREE SHIPPING ON ORDERS</span>
 			<span class="big-text">OVER $80</span>
 		</a>
