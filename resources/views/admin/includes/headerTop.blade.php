f5ac3d3483c2f330b9c44df8b3655c29b218ddae
Andreas Beasley <andreas@sapioweb.com>
2016-02-11 01:24:49 -0800

    todo updated, roles created, minor adjustments

--- resources/views/admin/includes/headerTop.blade.php
+++ resources/views/admin/includes/headerTop.blade.php
@@ -8,8 +8,11 @@
 
 	<ul class="nav navbar-nav navbar-right pull-right">
 		<li class="dropdown">
-			<a href="ecommerce-dashboard.html" class="dropdown-toggle waves-effect waves-light profile" data-toggle="dropdown" aria-expanded="true"><img src="{{url('admin-assets/assets/images/users/avatar-1.jpg')}}" alt="user-img" class="img-circle"> </a>
+			<a href="ecommerce-dashboard.html" class="dropdown-toggle waves-effect waves-light profile" data-toggle="dropdown" aria-expanded="true">
+				<img src="{{url('admin-assets/assets/images/users/avatar-1.jpg')}}" alt="user-img" class="img-circle">
+			</a>
 			<ul class="dropdown-menu">
+				<li><a href="{{url('/shop')}}">View Shop</a></li>
 				<li><a href="{{url('/logout')}}"><i class="ti-power-off m-r-5"></i> Logout</a></li>
 			</ul>
 		</li>
