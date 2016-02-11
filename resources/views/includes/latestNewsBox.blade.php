0e6775490bc3d770e175f99db9d4fcb90db59bed
Andreas Beasley <andreas@sapioweb.com>
2016-02-11 01:24:49 -0800

    todo updated, roles created, minor adjustments

--- resources/views/includes/latestNewsBox.blade.php
+++ resources/views/includes/latestNewsBox.blade.php
@@ -1,8 +1,8 @@
 <div class="box purple1">
 	<div class="simple-box">
-		<a href="index-2.html#" class="underline">
+		<a href="{{url('/shop')}}" class="underline">
 			<span class="small-text">CHECK THEKLIFF</span>
-			<span class="big-text">LATEST NEWS</span>
+			<span class="big-text">THE LATEST</span>
 		</a>
 	</div>
 </div>
