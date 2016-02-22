b3bbc69d1c89c17ec95f885f6a213ad8f2192dcd
Andreas Beasley <andreas@sapioweb.com>
2016-02-21 20:07:21 -0800

    is_main added to images table

---
+++ database/migrations/2016_02_18_181444_add_is_main_to_images_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsMainToImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->integer('is_main');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropColumn('is_main');
+        });
+    }
+}
