ba50dff1560aeb7e1fc51c180c754a41f5ba2d8d
Andreas Beasley <andreas@sapioweb.com>
2016-02-21 20:17:43 -0800

    categories controller update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -18,17 +18,29 @@ class CategoryController extends Controller
 
 	public function show(Request $request, $category)
 	{
-		$products = CrudHelper::relationshipQuery(new \App\Product, ['categories'], 'category', $category);
-
-		if (isset($request->query()['subcategory'])) {
-			// dd($request->query()['subcategory']);
-		}
-
-		$products = $products->get();
+		$products = CrudHelper::relationshipQuery(new \App\Product, ['categories'], 'category', $category)->with('images');
+		$products = $products->get()->toArray();
 
 		return view('shop.shop')->with([
 			'products' => $products,
 			'category' => $category
 		]);
+	}
+
+	public function getCategoryIds($categories)
+	{
+		foreach ($categories as $categoryKey => $categoryValue) {
+			if (is_null($categoryValue)) {
+				unset($categories[$categoryKey]);
+			}
+		}
+
+		foreach ($categories as $key => $value) {
+			$category = CrudHelper::show(new \App\Category, 'category', $value)->first();
+
+			$categoryIds[] = $category->id;
+		}
+
+		return $categoryIds;
 	}
 }
