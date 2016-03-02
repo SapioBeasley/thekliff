1699bd4b62af0e2802aa178efced6df731dbf79e
Andreas Beasley <andreas@sapioweb.com>
2016-03-02 00:53:08 -0800

    old laravel form helper replaced with new

--- config/app.php
+++ config/app.php
@@ -156,7 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Tail\TailServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -203,8 +203,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'=> Illuminate\Html\FormFacade::class,
-        'Html'=> Illuminate\Html\HtmlFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
         'CrudHelper' =>Sapioweb\CrudHelper\CrudyController::class,
 
     ],
